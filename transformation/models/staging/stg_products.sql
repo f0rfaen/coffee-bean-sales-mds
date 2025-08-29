85475c70952bfdf3249dd92797d6ea052aa05b47
Zackarias Madsen <zacke.madsen@gmail.com>
2025-08-29 13:13:44 +0200

    feat: added trino as sql query engine

--- transformation/models/staging/stg_products.sql
+++ transformation/models/staging/stg_products.sql
@@ -1,4 +1,5 @@
-{{ config(materialized = 'view', schema='staging') }}
+{{ config(materialized='table', schema='staging') }}
+
 SELECT
     CAST(product_id AS VARCHAR) AS product_id,
     CASE
@@ -15,10 +16,8 @@ SELECT
         ELSE TRIM(LOWER(roast_type))
     END AS roast_type,
     CAST(size AS INTEGER) AS size,
-    CAST(unit_price AS DECIMAL(10, 2)) AS unit_price,
-    CAST(price_per_100g AS DECIMAL(10, 2)) AS price_per_100g,
-    CAST(profit AS DECIMAL(10, 2)) AS profit,
+    CAST(unit_price AS DECIMAL(10,2)) AS unit_price,
+    CAST(price_per_100g AS DECIMAL(10,2)) AS price_per_100g,
+    CAST(profit AS DECIMAL(10,2)) AS profit
 FROM {{ source('raw', 'products') }}
-WHERE
-    product_id IS NOT NULL
-    
+WHERE product_id IS NOT NULL
